b6b01de1cb06a6fd9e15f04c5171586d45495a1b
Nate Sales <nate@natesales.net>
2021-03-29 23:35:54 -0700

    add API client

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Go API client for aarch64.com
+
+Example:
+
+```go
+package main
+
+import (
+	"fmt"
+	"github.com/natesales/aarch64-client-go/pkg/aarch64"
+	"log"
+)
+
+func main() {
+	client := aarch64.Client{APIKey: "cfea63484ccfea63484bfe78ed72d2cbfe78eea63484bfe78e"}
+
+	resp, err := client.Projects()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%+v", resp)
+}
+
+```
---
+++ pkg/aarch64/main.go
@@ -0,0 +1,148 @@
+package aarch64
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+// API Server
+const server = "https://console.aarch64.com/api"
+
+type Client struct {
+	APIKey string
+}
+
+type APIMeta struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+}
+
+type APIResponse struct {
+	Data interface{} `json:"data"`
+	Meta APIMeta     `json:"meta"`
+}
+
+type VM struct {
+	Id      string `json:"_id"`
+	Project string `json:"project"`
+	PoP     string `json:"pop"`
+	Host    uint   `json:"host"`
+	Index   uint   `json:"index"`
+
+	Hostname string `json:"hostname"`
+	VCPUs    uint   `json:"vcpus"`
+	Memory   uint   `json:"memory"`
+	Disk     uint   `json:"disk"`
+	OS       string `json:"os"`
+	Prefix   string `json:"prefix"`
+	Gateway  string `json:"gateway"`
+	Address  string `json:"address"`
+
+	Password string `json:"password"`
+
+	PhonedHome bool `json:"phoned_home"`
+}
+
+type Project struct {
+	Id    string   `json:"_id"`
+	Name  string   `json:"name"`
+	Users []string `json:"users"`
+	VMs   []VM     `json:"vms"`
+}
+
+type ProjectsResponse struct {
+	Meta     APIMeta   `json:"meta"`
+	Projects []Project `json:"data"`
+}
+
+func (c Client) req(method string, endpoint string, body interface{}, output interface{}) error {
+	var _body io.Reader
+
+	// If there is data in the body
+	if body != nil {
+		// Marshal body interface as JSON
+		jsonBody, err := json.Marshal(body)
+		if err != nil {
+			return err
+		}
+		_body = bytes.NewBuffer(jsonBody)
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, server+endpoint, _body)
+	if err != nil {
+		return err
+	}
+
+	// Set request headers
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", c.APIKey)
+
+	// Send the request
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(output); err != nil {
+		return err
+	}
+
+	return nil // nil error
+}
+
+func (c Client) Projects() (ProjectsResponse, error) {
+	var resp ProjectsResponse
+	if err := c.req("GET", "/projects", nil, &resp); err != nil {
+		return ProjectsResponse{}, err
+	}
+
+	return resp, nil
+}
+
+func (c Client) CreateProject(name string) (APIResponse, error) {
+	var resp APIResponse
+	if err := c.req("POST", "/project", map[string]string{"name": name}, &resp); err != nil {
+		return APIResponse{}, err
+	}
+
+	return resp, nil
+}
+
+func (c Client) AddUser(projectId string, email string) (APIResponse, error) {
+	var resp APIResponse
+	if err := c.req("POST", "/vms/adduser", map[string]string{
+		"project": projectId,
+		"email":   email,
+	}, &resp); err != nil {
+		return APIResponse{}, err
+	}
+
+	return resp, nil
+}
+
+func (c Client) CreateVM(hostname string, pop string, projectId string, plan string, os string) (APIResponse, error) {
+	var resp APIResponse
+	if err := c.req("POST", "/vms/create", map[string]string{
+		"hostname": hostname,
+		"pop":      pop,
+		"project":  projectId,
+		"plan":     plan,
+		"os":       os,
+	}, &resp); err != nil {
+		return APIResponse{}, err
+	}
+
+	return resp, nil
+}
+
+func (c Client) DeleteVM(vm string) (APIResponse, error) {
+	var resp APIResponse
+	if err := c.req("DELETE", "/vms/delete", map[string]string{"vm": vm}, &resp); err != nil {
+		return APIResponse{}, err
+	}
+
+	return resp, nil
+}
